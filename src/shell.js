86d1b65911448c9e456bcbeda03af5d9c57cd4c0
Rajkumar <b-rajkumar@users.noreply.github.com>
2023-04-28 11:48:06 +0530

    implemented ls,cd and pwd commands

--- src/shell.js
+++ src/shell.js
@@ -1,15 +1,22 @@
 const fs = require('fs');
 
-const ls = function(path = pwd) {
-  console.log(fs.readdirSync(path));
+const HOME = process.env.HOME;
+let PWD = process.env.PWD;
+
+const ls = function([path = PWD]) {
+  const resolvedPath = resolvePath(path);
+  console.log(`ls : ${resolvedPath}`);
+  console.log(fs.readdirSync(resolvedPath));
 };
 
 const pwd = function() {
-  console.log('pwd');
+  console.log(`pwd: ${PWD}`);
 };
 
-const cd = function() {
-  console.log('cd');
+const cd = function([path = '~']) {
+  const resolvedPath = resolvePath(path);
+  console.log(`cd : ${resolvedPath}`);
+  PWD = resolvedPath;
 };
 
 const tokenize = function(code) {
@@ -17,40 +24,59 @@ const tokenize = function(code) {
   return commands.split('\n').slice(0, -1);
 };
 
-const resolvePath = function(pwd, relativePath) {
+const resolvePath = function(relativePath) {
   const regx = /\.\./;
-  let path = pwd;
+
+  if(PWD === relativePath) {
+    return PWD;
+  }
+
+  if(/~/.test(relativePath)) {
+    relativePath = relativePath.replace(/~/, HOME);
+    return relativePath;
+  }
+
+  let path = PWD;
   relativePath = relativePath.replace(/^\.\/(.*)/, `$1`);
 
   while(regx.test(relativePath)) {
     path = path.replace(/(.*)\/(.*)$/, `$1`);
-    relativePath = relativePath.replace(/^\.\.\/(.*)/, `$1`);
+    relativePath = relativePath.replace(/^\.\.(.*)/, `$1`);
+    relativePath = relativePath.replace(/^\/(.*)/, `$1`);
   }
 
-  return `${path}/${relativePath}`;
+  let resolvedPath = path;
+  resolvedPath = `${path}/${relativePath}`;
+
+  return resolvedPath.replace(/(.*)\/\/(.*)/, `$1/$2`);
 };
 
 const commands = {ls, pwd, cd};
 
-const isValidCommand = function(command) {
-
+const isValidToken = function(token) {
+  return commands[token] !== undefined;
 };
 
-const executeCommands = function(token) {
+const executeCommand = function([command, ...args]) {
+  commands[command](args);
+};
+
+const parseTokens = function(tokens) {
   tokens.forEach(function(token) {
     const parsedToken = token.split(' ');
-    const command = token[0];
 
-
-
-  };
+    if(!isValidToken(parsedToken[0])) {
+      console.error(`${parsedToken[0]} : Invalid token!`);
+      process.exit(1);
+    }
+    executeCommand(parsedToken);
+  });
 };
 
 const run = function() {
   const code = fs.readFileSync('./src/shell-script.txt', 'utf-8');
   const tokens = tokenize(code);
-  const pwd = process.env.PWD;
-  executeCommands(tokens);
+  parseTokens(tokens);
 };
 
 run();
